428f3c3a77551685d377ac6f4444524bb041722d
Ramii Ahmed <ramy@ahmedramy.com>
2021-07-05 08:30:48 +0000

    Introduce high touch polling rate feature control
    Change-Id: I82c96d696e65d7c9cb345d37b071905a5d2f48de
    Signed-off-by: klozz <carlosj@klozz.dev>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5600,6 +5600,14 @@ public final class Settings {
         public static final String TOUCHSCREEN_GESTURE_HAPTIC_FEEDBACK = "touchscreen_gesture_haptic_feedback";
 
         /**
+         * Whether the HighTouchPollingRate is activated or not.
+         * 0 = off, 1 = on
+         * @hide
+         */
+        public static final String HIGH_TOUCH_POLLING_RATE_ENABLE =
+                "high_touch_polling_rate_enable";
+
+        /**
          * Whether the HighTouchSensitivity is activated or not.
          * 0 = off, 1 = on
          * @hide
@@ -5748,6 +5756,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(VIBRATE_ON_CALLWAITING);
             PRIVATE_SETTINGS.add(VIBRATE_ON_DISCONNECT);
             PRIVATE_SETTINGS.add(TOUCHSCREEN_GESTURE_HAPTIC_FEEDBACK);
+            PRIVATE_SETTINGS.add(HIGH_TOUCH_POLLING_RATE_ENABLE);
             PRIVATE_SETTINGS.add(HIGH_TOUCH_SENSITIVITY_ENABLE);
         }
 
