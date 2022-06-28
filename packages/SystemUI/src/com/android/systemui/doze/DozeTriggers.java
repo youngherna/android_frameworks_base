7dc1cf8556e23a1a4a9bcfd10308b76393b83246
Adithya R <gh0strider.2k18.reborn@gmail.com>
2022-06-28 09:55:33 +0530

    SystemUI: Check correct value for doze pickup sensor
    Some sensors report 2 when the device is put back down
    after picking up. Wake up the device only when the sensor
    reports 1, meaning picked up.
    
    Change-Id: I92bba201c61d49090f5ca645ee1fe9cf3459c3a8

--- packages/SystemUI/src/com/android/systemui/doze/DozeTriggers.java
+++ packages/SystemUI/src/com/android/systemui/doze/DozeTriggers.java
@@ -326,7 +326,7 @@ public class DozeTriggers implements DozeMachine.Part {
                         mDozeHost.onSlpiTap(screenX, screenY);
                     }
                     gentleWakeUp(pulseReason);
-                } else if (isPickup) {
+                } else if (isPickup && rawValues[0] == 1) {
                     if (shouldDropPickupEvent())  {
                         mDozeLog.traceSensorEventDropped(pulseReason, "keyguard occluded");
                         return;
