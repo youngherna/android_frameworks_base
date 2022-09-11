438bef450e11abe7ec66587d7d8e7826de3b6174
klozz <carlosj@klozz.dev>
2022-09-11 12:51:01 +0200

    fixup!
    Signed-off-by: klozz <carlosj@klozz.dev>
    Change-Id: I7a652d55852a199aeba1af522bacba67464284fa

--- core/java/com/android/internal/util/xperience/ThemeUtils.java
+++ core/java/com/android/internal/util/xperience/ThemeUtils.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.android.internal.util.evolution;
+package com.android.internal.util.xperience;
 
 import static android.os.UserHandle.USER_SYSTEM;
 
