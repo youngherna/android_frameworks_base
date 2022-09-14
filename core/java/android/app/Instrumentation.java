f6356f801979be010066f452391eef59c8bf51ac
klozz <carlosj@klozz.dev>
2022-09-14 12:40:21 +0200

    Revert "base: Spoof build fingerprint for Google Play Services [1/2]"
    This reverts commit b027a2aeb79937fd8fa84d9b2716c64a0af6d8a1.

--- core/java/android/app/Instrumentation.java
+++ core/java/android/app/Instrumentation.java
@@ -56,7 +56,6 @@ import android.view.Window;
 import android.view.WindowManagerGlobal;
 
 import com.android.internal.content.ReferrerIntent;
-import com.android.internal.gmscompat.AttestationHooks;
 
 import java.io.File;
 import java.lang.annotation.Retention;
@@ -1244,7 +1243,6 @@ public class Instrumentation {
         Application app = getFactory(context.getPackageName())
                 .instantiateApplication(cl, className);
         app.attach(context);
-        AttestationHooks.initApplicationBeforeOnCreate(app);
         return app;
     }
     
@@ -1262,7 +1260,6 @@ public class Instrumentation {
             ClassNotFoundException {
         Application app = (Application)clazz.newInstance();
         app.attach(context);
-        AttestationHooks.initApplicationBeforeOnCreate(app);
         return app;
     }
 
