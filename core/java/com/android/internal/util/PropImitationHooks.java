7b2c71d01f25f11f6ac8e077427ed58f4e7a102e
Adithya R <gh0strider.2k18.reborn@gmail.com>
2022-09-19 17:33:34 +0530

    PIHooks: Allow spoofing device model for Netflix
    It can be used to unlock HDR for devices that don't
    have it by default.
    
    Set config_netflixSpoofModel to the desired model in
    device overlay.
    
    Change-Id: Ibfa3c590ef2b388e18de2bb152114291623143b9
    
    Signed-off-by: klozz <carlosj@klozz.dev>
    Change-Id: Ibdff0771584f5574fa58706f7c222f5322507c0f

--- core/java/com/android/internal/util/PropImitationHooks.java
+++ core/java/com/android/internal/util/PropImitationHooks.java
@@ -82,6 +82,10 @@ public class PropImitationHooks {
     private static final boolean sSpoofGapps =
             Resources.getSystem().getBoolean(R.bool.config_spoofGoogleApps);
 
+    private static final String PACKAGE_NETFLIX = "com.netflix.mediaclient";
+    private static final String sNetflixModel =
+            Resources.getSystem().getString(R.string.config_netflixSpoofModel);
+
     private static volatile boolean sIsGms = false;
     private static volatile boolean sIsFinsky = false;
     private static volatile boolean sIsPhotos = false;
@@ -112,6 +116,9 @@ public class PropImitationHooks {
                 || packageName.equals(PACKAGE_WALLPAPERS))) {
             dlog("Spoofing Pixel 6 Pro for: " + packageName);
             sP6Props.forEach((k, v) -> setPropValue(k, v));
+        } else if (!sNetflixModel.isEmpty() && packageName.equals(PACKAGE_NETFLIX)) {
+            dlog("Setting model to " + sNetflixModel + " for Netflix");
+            setPropValue("MODEL", sNetflixModel);
         }
     }
 
