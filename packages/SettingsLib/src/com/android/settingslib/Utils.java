c974bf475be35222c1a30e7def14a74f5c7622af
klozz <carlosj@klozz.dev>
2023-03-21 23:46:32 -0600

    Display actual charge tech in battery settings [1/2]
    Turbo charge,VOOC charge, Warp charge, dash charge , Mi Turbo charge
    whichever one applies
    Co-authored-by: Anushek Prasal <anushekprasal@gmail.com>
    Change-Id: I5b157aad52dccf440d4dbbfa15b691cc1028edf2
    Signed-off-by: klozz <carlosj@klozz.dev>

--- packages/SettingsLib/src/com/android/settingslib/Utils.java
+++ packages/SettingsLib/src/com/android/settingslib/Utils.java
@@ -266,6 +266,21 @@ public class Utils {
                     statusString = res.getString(R.string.battery_info_status_charging);
                 } else if (batteryStatus.isPluggedInWired()) {
                     switch (batteryStatus.getChargingSpeed(context)) {
+                        case BatteryStatus.CHARGING_TURBO_POWER:
+                            statusString = res.getString(R.string.battery_info_status_turbo_charging);
+                            break;
+                        case BatteryStatus.CHARGING_VOOC:
+                            statusString = res.getString(R.string.battery_info_status_vooc_charging);
+                            break;
+                        case BatteryStatus.CHARGING_DASH:
+                            statusString = res.getString(R.string.battery_info_status_dash_charging);
+                            break;
+                        case BatteryStatus.CHARGING_WARP:
+                            statusString = res.getString(R.string.battery_info_status_warp_charging);
+                            break;
+                        case BatteryStatus.CHARGING_MI_TURBO_CHARGER:
+                            statusString = res.getString(R.string.battery_info_status_mi_turbo_charge);
+                            break;
                         case BatteryStatus.CHARGING_FAST:
                             statusString = res.getString(
                                     R.string.battery_info_status_charging_fast);
