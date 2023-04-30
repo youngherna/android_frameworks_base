3cec74f892197ed614da4164b2a9085a6f91b414
klozz <carlosj@klozz.dev>
2023-04-30 05:23:19 -0600

    PropImitationHooks: Update cheetah to april SLP
    Change-Id: Ibc6e91288ef02f1737f777bd1d11446b18f5870c
    Signed-off-by: klozz <carlosj@klozz.dev>

--- core/java/com/android/internal/util/PropImitationHooks.java
+++ core/java/com/android/internal/util/PropImitationHooks.java
@@ -92,7 +92,7 @@ public class PropImitationHooks {
         sP7Props.put("DEVICE", "cheetah");
         sP7Props.put("PRODUCT", "cheetah");
         sP7Props.put("MODEL", "Pixel 7 Pro");
-        sP7Props.put("FINGERPRINT", "google/cheetah/cheetah:13/TQ2A.230305.008.C1/9619669:user/release-keys");
+        sP7Props.put("FINGERPRINT", "google/cheetah/cheetah:13/TQ1A.230205.001.D2/9471403:user/release-keys");
     }
 
     private static final boolean sSpoofGapps =
