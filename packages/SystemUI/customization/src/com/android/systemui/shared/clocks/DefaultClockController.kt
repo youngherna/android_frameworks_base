c570cbdee11459bc5d39175474da83d826a86502
klozz <carlosj@klozz.dev>
2023-04-30 07:20:58 -0600

    fix duplications for clock spacing commit
    Change-Id: I0b334cf179d1789ffe03ea40b9800b66ba0ae6ee
    Signed-off-by: klozz <carlosj@klozz.dev>

--- packages/SystemUI/customization/src/com/android/systemui/shared/clocks/DefaultClockController.kt
+++ packages/SystemUI/customization/src/com/android/systemui/shared/clocks/DefaultClockController.kt
@@ -35,8 +35,6 @@ import java.io.PrintWriter
 import java.util.Locale
 import java.util.TimeZone
 
-import com.android.internal.util.xpe.xperienceUtils
-
 private val TAG = DefaultClockController::class.simpleName
 
 /**
